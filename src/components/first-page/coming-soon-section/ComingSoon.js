8c67f10e7d21ad8529b14100364f3f7fd697f143
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-05-22 13:53:28 +0300

    added animation to first page buttons and improved media queries

--- src/components/first-page/coming-soon-section/ComingSoon.js
+++ src/components/first-page/coming-soon-section/ComingSoon.js
@@ -5,13 +5,13 @@ import Grid from "@material-ui/core/Grid";
 const ComingSoon = (props) => {
     return (
         <Grid container direction="row" className="coming-soon-section">
-            <Grid container item sm={5} className="coming-soon">
+            <Grid container item md={5} sm={12} className="coming-soon">
                 <p>Coming soon</p>
             </Grid>
-            <Grid container item sm={4} className="coming-soon-text">
+            <Grid container item md={4} sm={8} className="coming-soon-text">
                 <p>We are planing to open a new service,<br/> where your cats and dogs can find their love!</p>
             </Grid>
-            <Grid container item sm={3} className="love">
+            <Grid container item md={3} sm={4} className="love">
                 <i className="far fa-heart"/>
                 <p>Love</p>
             </Grid>
--- src/components/first-page/welcome-section/WelcomeSection.js
+++ src/components/first-page/welcome-section/WelcomeSection.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import './welcomeSection.scss';
 import loop from "../../../img/icons/loop.svg";
 import Grid from "@material-ui/core/Grid";
 
 const WelcomeSection = (props) => {
     const { setForm, form } = props;
+    const [btnTextLost, setBtnText] = useState('I lost my pet');
+    const [btnTextFound, setBtnTextFound] = useState('I found a pet');
+    const [hoveredLost, setHoverLost] = useState(false);
+    const [hoveredFound, setHoverFound] = useState(false);
+    const [hoveredLostBack, setHoverLostBack] = useState(false);
+    const [hoveredFoundBack, setHoverFoundBack] = useState(false);
 
     return (
         <Grid container direction="row" className={form ? "welcome-section under-form" : "welcome-section"}>
@@ -14,14 +20,33 @@ const WelcomeSection = (props) => {
                 community</p>
                 <div className="i-lost-pet" onClick={() => {
                     setForm(true);
-                }}>
-                    <span className="i-lost-pet-text">I lost my pet</span>
+                }} onMouseEnter={() => {
+                    setHoverLost(true);
+                    setHoverLostBack(false);
+                    setBtnText('Click to find!');
+                }}
+                     onMouseLeave={() => {
+                         setHoverLostBack(true);
+                         setHoverLost(false);
+                         setBtnText('I lost my pet');
+                     }}>
+                    <span className={hoveredLost ? "i-lost-pet-text animated" :
+                        hoveredLostBack ? "i-lost-pet-text animated-back" : "i-lost-pet-text"}>{btnTextLost}</span>
                     <img src={loop} alt="loop"/>
                 </div>
                 <div className="i-found-pet" onClick={() => {
                     setForm(true);
+                }} onMouseEnter={() => {
+                    setHoverFound(true);
+                    setHoverFoundBack(false);
+                    setBtnTextFound('What to do?');
+                }}
+                onMouseLeave={() => {
+                    setHoverFound(false);
+                    setHoverFoundBack(true);
+                    setBtnTextFound('I found a pet');
                 }}>
-                    <span>I found a pet</span>
+                    <span className={hoveredFound ? "animated" : hoveredFoundBack ? "animated-back" : ""}>{btnTextFound}</span>
                 </div>
                 <p className="join">I'm okay, just want to <span>JOIN</span> the pawsome community!</p>
             </Grid>
