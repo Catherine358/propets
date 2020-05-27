714d37dfb72f5da22851752595ca07c833192992
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-05-27 18:42:23 +0300

    finished profile page

--- src/components/other-pages/profile-page/profile/Profile.js
+++ src/components/other-pages/profile-page/profile/Profile.js
@@ -2,11 +2,14 @@ import React, {useState} from "react";
 import './profile.scss';
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import PetInfo from "../../lost-page/main-block/pet-info";
+import Activity from "../activities";
 
 const pic = "https://sun6-16.userapi.com/E-J3H_cTJO9xr3DE2g_g_6vazm5YPD-gGqeqIQ/7Z1WCb9JJiw.jpg";
 
 const Profile = (props) => {
     const [info, setInfo] = useState(false);
+    const [inputName, setInput] = useState(false);
 
     return (
         <Grid container direction="row" className="profile-main-block">
@@ -22,11 +25,19 @@ const Profile = (props) => {
                                 setInfo(true);
                             }}>Activities</span>
                         </div>
-                        <div className="profile-info">
+                        {info && <Activity inactive={false}/>}
+                        {info && <div className="deactivated-text">
+                            <p>The post was deactivated on 12 Dec, 2019. Click here to re-activate it.</p>
+                        </div>}
+                        {info && <Activity inactive={true}/>}
+                        {!info && <div className="profile-info">
                             <div className="profile-info-picture">
                                 <img src={pic} alt="profile-picture"/>
-                                <p>Anna Smith</p>
-                                <i className="fas fa-pencil-alt"/>
+                                {!inputName && <p>Anna Smith</p>}
+                                {inputName && <input type="text" name="name"/>}
+                                <i className="fas fa-pencil-alt" onClick={() => {
+                                    setInput(true);
+                                }}/>
                             </div>
                             <fieldset>
                                 <div className="profile-labels">
@@ -40,13 +51,13 @@ const Profile = (props) => {
                                     <input type="link" name="fb-link"/>
                                 </div>
                             </fieldset>
-                        </div>
+                        </div>}
                     </Grid>
-                    <div className="profile-buttons">
+                    {!info && <div className="profile-buttons">
                         <Button variant="contained" className="profile-btn-cancel">Cancel</Button>
                         <Button className="profile-submit-btn" variant="contained" type="submit">
                             <i className="fas fa-save"/>Save changes</Button>
-                    </div>
+                    </div>}
                 </form>
             </Grid>
             <Grid container item lg={4} className="profile-right-side"/>
