358d9345427ecc9ff8093940f7572c98f6f27751
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-07-13 14:52:35 +0300

    Updates

---
+++ precache-manifest.ab51ab06070ad3439dba9b417b333931.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46a832f2c9f446cb61ca40a20b4f6a4",
+    "url": "/propets/index.html"
+  },
+  {
+    "revision": "3b577577afdd7b409ddd",
+    "url": "/propets/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1fe0c68b43dc1c448464",
+    "url": "/propets/static/css/main.f0c9f185.chunk.css"
+  },
+  {
+    "revision": "3b577577afdd7b409ddd",
+    "url": "/propets/static/js/2.175ef753.chunk.js"
+  },
+  {
+    "revision": "30549bbedba7c7d7fba393012280321f",
+    "url": "/propets/static/js/2.175ef753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe0c68b43dc1c448464",
+    "url": "/propets/static/js/main.d987732b.chunk.js"
+  },
+  {
+    "revision": "053a8cc34f1a68100ab6",
+    "url": "/propets/static/js/runtime-main.648257b3.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/propets/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/propets/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/propets/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "9c49ad04f7b554066f2561ee129d076e",
+    "url": "/propets/static/media/Screenshot_Scale.9c49ad04.png"
+  },
+  {
+    "revision": "8696757d69413026c1f014f5683059c3",
+    "url": "/propets/static/media/dog.8696757d.png"
+  },
+  {
+    "revision": "212a01b5f39d79f9a33966380b06f619",
+    "url": "/propets/static/media/logo.212a01b5.svg"
+  },
+  {
+    "revision": "31348d6e6c68ff2952fa7b7e7fb6e3f1",
+    "url": "/propets/static/media/logo_green.31348d6e.svg"
+  },
+  {
+    "revision": "f610f6d230672104cd8d92ac75401b1c",
+    "url": "/propets/static/media/loop.f610f6d2.svg"
+  },
+  {
+    "revision": "65547223c74f7f3c42051f9d98461c21",
+    "url": "/propets/static/media/pet_photo.65547223.png"
+  },
+  {
+    "revision": "6060af52f8eaa27e89602170c7ed24c8",
+    "url": "/propets/static/media/pets.6060af52.png"
+  }
+]);
